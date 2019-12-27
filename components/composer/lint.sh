23f44d820662544dbb96673030d10171a20be448
Matej Košiarčik <matej.kosiarcik@gmail.com>
2019-12-28 00:11:46 +0100

    feat: add composer package validation

--- components/composer/lint.sh
+++ components/composer/lint.sh
@@ -3,3 +3,4 @@ set -euf
 cd "${WORKDIR}"
 
 git ls-files -z '*.json' 'composer.lock' '*/composer.lock' '*.htmlhintrc' '*.babelrc' | xargs -0 jsonlint
+git ls-files -z 'composer.json' '*/composer.json' | xargs -0 composer validate
