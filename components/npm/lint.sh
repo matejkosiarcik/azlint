541347febc93771f6b0c8cf520a1cb5d72ce3f84
Matej Košiarčik <matej.kosiarcik@gmail.com>
2019-12-28 00:31:48 +0100

    style: make json and markdown linting before linting specific files

--- components/npm/lint.sh
+++ components/npm/lint.sh
@@ -3,9 +3,9 @@ set -euf
 cd "${WORKDIR}"
 
 git ls-files -z | xargs -0 eclint check
+git ls-files -z '*.md' '*.markdown' '*.mdown' '*.mdwn' '*.mdx' '*.mkd' '*.mkdn' '*.mkdown' '*.ronn' '*.workbook' 'contents.lr' '*/contents.lr' | xargs -0 markdownlint
+git ls-files -z '*.json' 'composer.lock' '*/composer.lock' | xargs -0 jsonlint --quiet
 git ls-files -z '*.sh' '*.bash' '*.zsh' '*.ksh' '*.mksh' '*.bats' | xargs -0 shellcheck -x
 git ls-files -z 'package.json' '*/package.json' | xargs -0 -n1 pjv --quiet --filename
 git ls-files -z '*.gitlab-ci.yml' | xargs -0 -n1 gitlab-ci-lint
 git ls-files -z '*.gitlab-ci.yml' | xargs -0 -n1 gitlab-ci-validate validate
-git ls-files -z '*.md' '*.markdown' '*.mdown' '*.mdwn' '*.mdx' '*.mkd' '*.mkdn' '*.mkdown' '*.ronn' '*.workbook' 'contents.lr' '*/contents.lr' | xargs -0 markdownlint
-git ls-files -z '*.json' 'composer.lock' '*/composer.lock' | xargs -0 jsonlint --quiet
