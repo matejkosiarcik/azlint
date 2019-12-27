6902aa8f27cce2f33282174bb363477659f4b008
Matej Košiarčik <matej.kosiarcik@gmail.com>
2019-12-27 23:48:21 +0100

    feat: add markdown validator

--- components/npm/lint.sh
+++ components/npm/lint.sh
@@ -7,3 +7,4 @@ git ls-files -z '*.sh' '*.bash' '*.zsh' '*.ksh' '*.mksh' '*.bats' | xargs -0 she
 git ls-files -z 'package.json' | xargs -0 -n1 pjv --filename
 git ls-files -z '.gitlab-ci.yml' | xargs -0 -n1 gitlab-ci-lint
 git ls-files -z '.gitlab-ci.yml' | xargs -0 -n1 gitlab-ci-validate validate
+git ls-files -z '*.md' '*.markdown' | xargs -0 markdownlint
