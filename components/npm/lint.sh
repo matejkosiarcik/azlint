55c0ee7e938637691073006feb4c5b717d17b79f
Matej Košiarčik <matej.kosiarcik@gmail.com>
2019-12-28 00:55:43 +0100

    feat: add more json files with possible comments included

--- components/npm/lint.sh
+++ components/npm/lint.sh
@@ -4,7 +4,7 @@ cd "${WORKDIR}"
 
 git ls-files -z | xargs -0 eclint check
 git ls-files -z '*.md' '*.markdown' '*.mdown' '*.mdwn' '*.mdx' '*.mkd' '*.mkdn' '*.mkdown' '*.ronn' '*.workbook' 'contents.lr' '*/contents.lr' | xargs -0 markdownlint
-git ls-files -z '*.json' 'composer.lock' '*/composer.lock' | xargs -0 jsonlint --quiet
+git ls-files -z '*.json' '*.geojson' '*.htmlhintrc' '*.babelrc' '*.jsonl' '*.eslintrc.json' '*.jscsrc' '*.jshintrc' '*.jshintrc' 'composer.lock' '*/composer.lock' | xargs -0 jsonlint --quiet --comments
 git ls-files -z '*.sh' '*.bash' '*.zsh' '*.ksh' '*.mksh' '*.bats' | xargs -0 shellcheck -x
 git ls-files -z 'package.json' '*/package.json' | xargs -0 -n1 pjv --quiet --filename
 git ls-files -z '*.gitlab-ci.yml' | xargs -0 -n1 -t gitlab-ci-lint
