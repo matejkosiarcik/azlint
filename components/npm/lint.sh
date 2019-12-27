a4933409691f6c65fef74a76a4c9da76af228879
Matej Košiarčik <matej.kosiarcik@gmail.com>
2019-12-27 23:48:10 +0100

    feat: add package.json and .gitlab-ci.yml validators

--- components/npm/lint.sh
+++ components/npm/lint.sh
@@ -4,3 +4,6 @@ cd "${WORKDIR}"
 
 git ls-files -z | xargs -0 eclint check
 git ls-files -z '*.sh' '*.bash' '*.zsh' '*.ksh' '*.mksh' '*.bats' | xargs -0 shellcheck -x
+git ls-files -z 'package.json' | xargs -0 -n1 pjv --filename
+git ls-files -z '.gitlab-ci.yml' | xargs -0 -n1 gitlab-ci-lint
+git ls-files -z '.gitlab-ci.yml' | xargs -0 -n1 gitlab-ci-validate validate
