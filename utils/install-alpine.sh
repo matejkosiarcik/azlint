473bf8490bc8b03e40c5668e5f69a362d8f59949
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-03-28 21:01:08 +0100

    fix: python3 installation

--- utils/install-alpine.sh
+++ utils/install-alpine.sh
@@ -12,7 +12,7 @@ apk update
 apk add alpine-sdk build-base
 apk add bash dash mksh loksh zsh yash
 apk add nodejs npm
-apk add python3 python3-dev py3-setuptools py3-virtualenv
+apk add python3 python3-dev py3-virtualenv
 apk add composer
 apk add go
 apk add libxml2-utils
