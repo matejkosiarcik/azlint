a8c4cc66079fc6ef59d9e66b38edd42a961b33b1
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-05-30 13:24:50 +0200

    chore: shell scripts tweaks

--- .ci/build.sh
+++ .ci/build.sh
@@ -6,9 +6,9 @@ if [ -z "${AZLINT_VERSION+x}" ]; then
     AZLINT_VERSION='dev'
 fi
 
-docker build '.' -t "matejkosiarcik/azlint:${AZLINT_VERSION}-all"
-docker build './components/shellcheck' -t "matejkosiarcik/azlint:${AZLINT_VERSION}-shellcheck"
-docker build './components/python' -t "matejkosiarcik/azlint:${AZLINT_VERSION}-python"
-docker build './components/composer' -t "matejkosiarcik/azlint:${AZLINT_VERSION}-composer"
-docker build './components/go' -t "matejkosiarcik/azlint:${AZLINT_VERSION}-go"
-docker build './components/node' -t "matejkosiarcik/azlint:${AZLINT_VERSION}-node"
+docker build --no-cache --pull '.' -t "matejkosiarcik/azlint:${AZLINT_VERSION}-all"
+docker build --no-cache --pull './components/shellcheck' -t "matejkosiarcik/azlint:${AZLINT_VERSION}-shellcheck"
+docker build --no-cache --pull './components/python' -t "matejkosiarcik/azlint:${AZLINT_VERSION}-python"
+docker build --no-cache --pull './components/composer' -t "matejkosiarcik/azlint:${AZLINT_VERSION}-composer"
+docker build --no-cache --pull './components/go' -t "matejkosiarcik/azlint:${AZLINT_VERSION}-go"
+docker build --no-cache --pull './components/node' -t "matejkosiarcik/azlint:${AZLINT_VERSION}-node"
--- components/node/main.sh
+++ components/node/main.sh
@@ -14,17 +14,16 @@ elif (xargs -r <'/dev/null' >'/dev/null' 2>&1); then
     xargs_r='-r'
 fi
 
-# disable indent size for eclint, because sometimes it is beneficial to disregard perfect indent size
+# disable indent size for editorconfig-lint, because sometimes it is beneficial to disregard perfect indent size
 # such as with properties in HTML elements or arguments in C functions, example:
 # <a prop1="foo"
 #    prop2="bar">
 #    ^ this is aligned with previous property, so it ends up 3 spaces from original indent level, which is unbalanced
 tr '\n' '\0' <'/projectlist/projectlist.txt' | xargs -0 ${xargs_r} eclint check --indent_size 1
 
-grep -iE '(\.(md|markdown|mdown|mdwn|mdx|mkd|mkdn|mkdown|ronn|workbook))|((^|/)contents.lr)$' <'/projectlist/projectlist.txt' | tr '\n' '\0' | xargs -0 ${xargs_r} markdownlint
-# git ls-files -z '*.json' '*.geojson' '*.htmlhintrc' '*.htmllintrc' '*.babelrc' '*.jsonl' '*.jscsrc' '*.jshintrc' '*.jslintrc' 'composer.lock' '*/composer.lock' | xargs -0 ${xargs_r} jsonlint --quiet --comments
-# git ls-files -z '*.sh' '*.ksh' '*.bash' '*.zsh' '*.bats' | xargs -0 ${xargs_r} shellcheck -x
-# git ls-files -z '*.bats' | xargs -0 -I% ${xargs_r} bats --count % >/dev/null
-# # git ls-files -z 'package.json' '*/package.json' | xargs -0 -n1 ${xargs_r} pjv --quiet --filename
-# git ls-files -z '*.gitlab-ci.yml' | xargs -0 -n1 ${xargs_r} gitlab-ci-lint
-# git ls-files -z '*.gitlab-ci.yml' | xargs -0 -n1 ${xargs_r} gitlab-ci-validate validate
+grep -iEe '\.(md|markdown|mdown|mdwn|mdx|mkd|mkdn|mkdown|ronn|workbook)$' -e '(^|/)contents\.lr$' <'/projectlist/projectlist.txt' | tr '\n' '\0' | xargs -0 ${xargs_r} markdownlint
+grep -iEe '\.(json|geojson|htmlhintrc|htmllintrc|babelrc|jsonl|jscsrc|jshintrc|jslintrc)$' -e '(^|/)composer\.lock$' <'/projectlist/projectlist.txt' | tr '\n' '\0' | xargs -0 ${xargs_r} jsonlint --quiet --comments
+grep -iE '\.bats$' <'/projectlist/projectlist.txt' | tr '\n' '\0' | xargs -0 -I% ${xargs_r} bats --count % >/dev/null
+# grep -iE '(^|/)package\.json$' <'/projectlist/projectlist.txt' | xargs -0 -n1 ${xargs_r} pjv --quiet --filename # TODO: enable for non-private packages
+grep -iE '\.gitlab-ci.yml$' <'/projectlist/projectlist.txt' | tr '\n' '\0' | xargs -0 -n1 ${xargs_r} gitlab-ci-lint
+grep -iE '\.gitlab-ci.yml$' <'/projectlist/projectlist.txt' | tr '\n' '\0' | xargs -0 -n1 ${xargs_r} gitlab-ci-validate validate
