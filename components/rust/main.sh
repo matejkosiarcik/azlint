75d4babc0e3155c7b86ef5c2acef9efd5da68eeb
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-07-20 14:59:33 +0200

    style: fix unused variables

--- components/rust/main.sh
+++ components/rust/main.sh
@@ -4,9 +4,9 @@ cd '/project'
 
 # Default in GNU xargs is to execute always
 # But not all xargs have this flag
-xargs_r=''
-if (xargs --no-run-if-empty <'/dev/null' >'/dev/null' 2>&1); then
-    xargs_r='--no-run-if-empty'
-elif (xargs -r <'/dev/null' >'/dev/null' 2>&1); then
-    xargs_r='-r'
-fi
+# xargs_r=''
+# if (xargs --no-run-if-empty <'/dev/null' >'/dev/null' 2>&1); then
+#     xargs_r='--no-run-if-empty'
+# elif (xargs -r <'/dev/null' >'/dev/null' 2>&1); then
+#     xargs_r='-r'
+# fi
