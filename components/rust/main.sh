25734e5cc05acca75733c7be9c2b8bc4371fa88c
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-07-21 23:51:19 +0200

    fix: rust json checking

--- components/rust/main.sh
+++ components/rust/main.sh
@@ -4,11 +4,20 @@ cd '/project'
 
 # Default in GNU xargs is to execute always
 # But not all xargs have this flag
-xargs_r=''
-if (xargs --no-run-if-empty <'/dev/null' >'/dev/null' 2>&1); then
-    xargs_r='--no-run-if-empty'
-elif (xargs -r <'/dev/null' >'/dev/null' 2>&1); then
-    xargs_r='-r'
-fi
+# xargs_r=''
+# if (xargs --no-run-if-empty <'/dev/null' >'/dev/null' 2>&1); then
+#     xargs_r='--no-run-if-empty'
+# elif (xargs -r <'/dev/null' >'/dev/null' 2>&1); then
+#     xargs_r='-r'
+# fi
 
-grep -iEe '\.json$' -e '(^|/)composer\.lock$' <'/projectlist/projectlist.txt' | tr '\n' '\0' | xargs -0 ${xargs_r} jsonprima
+tmpfile="$(mktemp)"
+grep -iEe '\.json$' -e '(^|/)composer\.lock$' <'/projectlist/projectlist.txt' | while read -r file; do
+    jsonprima --input "$(cat "${file}")" >"${tmpfile}"
+    if [ "$(cat "${tmpfile}")" != '[]' ]; then
+        printf '%s - ' "${file}"
+        cat "${tmpfile}"
+        exit 1
+    fi
+done
+rm -rf "${tmpfile}"
