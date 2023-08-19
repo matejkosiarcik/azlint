dab0e44460a0873b617e2d7c72282a408a325a70
Matej Košiarčik <matej.kosiarcik@gmail.com>
2023-08-19 09:50:21 +0200

    test: fix flaky delay test

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -22,10 +22,10 @@ it('TEMPDIR is not git repo', async function () {
 
 it('Delay', async function () {
     const start = Date.now();
-    await delay(20);
+    await delay(15);
     const end = Date.now();
     const difference = (end - start) / 1000;
-    expect(difference, 'Delay should be delayed').gte(0.02).lte(0.1);
+    expect(difference, 'Delay should be delayed').gte(0.01).lte(0.1);
 });
 
 it('Resolve promises', async function () {
