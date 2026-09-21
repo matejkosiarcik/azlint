98391e188b379115f88ddb7d711d624378cbfc40
Matej Košiarčik <matej.kosiarcik@gmail.com>
2026-09-21 17:13:05 +0200

    chore: ignore some shellcheck errors

--- utils/rust/get-target-arch.sh
+++ utils/rust/get-target-arch.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -euf
 
+# shellcheck disable=SC2154
 case "${TARGETARCH}" in
 arm64)
     printf 'aarch64\n'
@@ -11,6 +12,7 @@ amd64)
     ;;
 
 *)
+    # shellcheck disable=SC2154
     printf 'Unsupported architecture: %s\n' "${TARGETARCH}"
     exit 1
     ;;
